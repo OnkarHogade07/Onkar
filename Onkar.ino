23df5cfe915a383b89c770b5eaa7b0029f74bdae
OnkarHogade07 <hogadeonkar07@gmail.com>
2024-09-15 09:07:56 +0530

    Put_Code

---
+++ Onkar.ino
@@ -0,0 +1,21 @@
+// Define the LED pin (You can change the pin number if your LED is connected to a different GPIO)
+int ledPin = 2;  // Most ESP32 boards have an onboard LED connected to GPIO 2
+
+void setup() {
+  // Set the LED pin as an output
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // Turn the LED on (HIGH is the voltage level)
+  digitalWrite(ledPin, HIGH);
+  
+  // Wait for 1 second
+  delay(1000); 
+  
+  // Turn the LED off by making the voltage LOW
+  digitalWrite(ledPin, LOW); 
+  
+  // Wait for 1 second
+  delay(1000);
+}
